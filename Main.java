b451137513e9d635a2b74960746335ad719fb93c
benjamin.oluyomi <boluyomi@academytrainee.com>
2020-11-05 10:17:36 +0000

    working java calc

---
+++ Main.java
@@ -0,0 +1,34 @@
+package est2;
+
+public class Main {
+	public static void addition(int a,int b) {
+		
+		int result = a + b;
+		System.out.println(result);
+	}
+	public static void subtraction(int a,int b) {
+		
+		int result = a - b;
+		System.out.println(result);
+	}
+	public static void multiplication(int a,int b) {
+	
+	int result = a * b;
+	System.out.println(result);
+}
+	public static void division(int a,int b) {
+		
+		int result = a / b;
+		System.out.println(result);
+	}
+	
+	
+	public static void main(String[] args) {
+		addition(5,5);
+		subtraction(5,5);
+		multiplication(5,5);
+		division(5,5);
+		
+		
+}
+}
